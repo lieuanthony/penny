45b3d95c935d41bd46b371898274f6c8ee339c05
Anthony Lieu <anthonyjunlieu@gmail.com>
2025-09-02 14:44:48 -0400

    add spring boot project template

---
+++ penny-backend/src/main/java/com/penny/backend/PennyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.penny.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PennyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PennyBackendApplication.class, args);
+	}
+
+}
---
+++ penny-frontend/.angular/cache/20.2.1/penny/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,37 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRendering,
+  setAngularAppEngineManifest,
+  setAngularAppManifest,
+  withAppShell,
+  withRoutes
+} from "./chunk-AZDA4CWG.js";
+import "./chunk-PVCWESX6.js";
+import "./chunk-3OLK3LQ5.js";
+import "./chunk-JXZMJYLS.js";
+import "./chunk-6DU2HRTW.js";
+export {
+  AngularAppEngine,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRendering,
+  withAppShell,
+  withRoutes,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};

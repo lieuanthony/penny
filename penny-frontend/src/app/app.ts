87ce9aaf1aa9b459798dbe2b7665c1c98cbd2619
Anthony Lieu <anthonyjunlieu@gmail.com>
2025-09-04 14:46:38 -0400

    create sign up and sign in components

--- penny-frontend/src/app/app.ts
+++ penny-frontend/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SignUp } from "./sign-up/sign-up";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SignUp],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
